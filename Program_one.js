ac48ef384512ac5c4fc5034a33d216fb90cfb75d
Leena <leenapriya1503@gmail.com>
2025-02-05 18:46:23 +0530

    first program

---
+++ Program_one.js
@@ -0,0 +1,17 @@
+let obj={
+    name : "guvi",
+    age : 10
+}
+let jsonobj={
+   " name" : "guvi",
+    "age" : 10
+}
+console.log(jsonobj)
+console.log("_______________")
+let jsonString = JSON.stringify(obj)
+console.log(jsonString)
+console.log(jsonString.name)
+console.log("________________")
+let jsonmethodparse = JSON.parse(jsonString)
+console.log(jsonmethodparse)
+console.log(jsonmethodparse.name)
